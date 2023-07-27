afb7de711aa370ff0f83662f4f26c9e563e5e5e5
DaredFTW <davferg43@gmail.com>
2023-07-27 18:58:04 -0400

    hover styling on profile page buttons

--- vue/src/views/Profile.vue
+++ vue/src/views/Profile.vue
@@ -189,6 +189,9 @@ h2 {
   font-family: "Mouse Memoirs", sans-serif;
   font-size: 1rem;
 }
+.addbook_btn:hover {
+    background-color: #4a305e;
+}
 #filter_form {
     background-color: #3a2649;
 }
